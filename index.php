879a757d62068b3524589d72bd5805c63a0cc561
Adrián Sierra Fernández <146702356+adri230@users.noreply.github.com>
2023-10-09 09:46:42 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tarea11</title
+    </head>
+    <body>
+        <?php
+           function nacimiento($nombre, $lugar, $año){
+               echo $nombre, " nació en ", $lugar, " en el año ", $año;
+           }
+           nacimiento("Alex","Madrid", 2000)
+           ?>
+    </body>
+</html>
